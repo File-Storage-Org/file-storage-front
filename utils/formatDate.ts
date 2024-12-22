34d043004c17567d799e127e066fcf6653d2e2a0
vitossss <shvab7216@gmail.com>
2024-12-22 15:25:25 +0200

    fix: add Kyiv formatter to date func

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,9 +1,18 @@
 const formatDate = (dateStr: string | Date): string | null => {
-  const date = new Date(dateStr);
-  const now = new Date();
+  const creating_date = new Date(new Date(dateStr).getTime() + 2 * 60 * 60 * 1000);
 
-  // Time difference in milliseconds
-  const timeDifference = now.getTime() - date.getTime();
+  const kyivFormatter = new Intl.DateTimeFormat("en-US", {
+    timeZone: "Europe/Kyiv",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  const nowInKyiv = new Date(kyivFormatter.format(new Date()));
+
+  const timeDifference = nowInKyiv.getTime() - creating_date.getTime();
   const secondsDifference = Math.floor(timeDifference / 1000);
   const minutesDifference = Math.floor(secondsDifference / 60);
   const hoursDifference = Math.floor(minutesDifference / 60);
